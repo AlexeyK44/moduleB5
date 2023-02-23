a1d9d9aed484eb4b92a89822e56efbb694580df0
AlexeyK44 <alesha.karpychev@gmail.com>
2023-02-24 01:05:09 +0300

    added game-projekt

---
+++ Game_X0.py
@@ -0,0 +1,92 @@
+def greet():
+    print("-" * 20)
+    print("  Приветствуем вас  ")
+    print("       в игре       ")
+    print("   крестики-нолики  ")
+    print("-" * 20)
+    print("  форма ввода: x y  ")
+    print("  x - номер строки  ")
+    print("  y - номер столбца ")
+
+
+def show():
+    print()
+    print("    | 0 | 1 | 2 |")
+    print(" ----------------  ")
+    for i, row in enumerate(field):
+        row_str = f"  {i} | {' | '.join(row)} | "
+        print(row_str)
+        print(" ----------------  ")
+    print()
+
+def ask():
+    while True:
+        cords = input("         Ваш ход: ").split()
+
+        if len(cords) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+
+        x, y = cords
+
+        if not(x.isdigit()) or not(y.isdigit()):
+            print(" Введите числа! ")
+            continue
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(" Неверный ввод координат! ")
+            continue
+
+        if field[x][y] != "-":
+            print(" Клетка занята! ")
+            continue
+
+        return x, y
+
+def check_win():
+    victories = [((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                 ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                 ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2))]
+
+    for cord in victories:
+        symbol = []
+
+        for c in cord:
+            symbol.append(field[c[0]][c[1]])
+
+        if symbol == ["X", "X", "X"]:
+            print(" Победу одержал Х !!! ")
+            return True
+        if symbol == ["0", "0", "0"]:
+            print(" Победу одержал 0 !!! ")
+            return True
+    return False
+
+greet()
+
+field = [["-"] * 3 for i in range(3)]
+
+count = 0
+while True:
+    count += 1
+    show()
+    if count % 2 == 1:
+        print(" Ходит Крестик")
+    else:
+        print(" Ходит Нолик")
+
+    x, y = ask()
+
+    if count % 2 == 1:
+       field[x][y] = "X"
+    else:
+        field[x][y] = "0"
+
+    if check_win():
+        break
+
+    if count == 9:
+        print(" Ничья! ")
+        break
+
